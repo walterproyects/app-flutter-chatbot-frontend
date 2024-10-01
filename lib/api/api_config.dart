7bc8250f5a5034f62eb7376bf44a3625b2329824
SebaschaM <schaquila@autonoma.edu.pe>
2024-10-01 02:32:48 -0500

    api release

--- lib/api/api_config.dart
+++ lib/api/api_config.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
-  static const String baseUrl = 'http://10.0.2.2:3000/api'; // Cambiar localhost por 10.0.2.2
+  static const String baseUrl = 'https://app-flutter-chatbot-backend.onrender.com/api'; // Cambiar localhost por 10.0.2.2
 
   //static const String baseUrl = 'https://xxxxxxx.dominio.com/api';
 }
